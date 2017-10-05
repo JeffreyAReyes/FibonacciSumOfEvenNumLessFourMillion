3f24a28e0fa1dd810cfa89ab46f1f74d6e0eeb80
jeffrey a reyes <jeofadfriel@hotmail.com>
2017-10-05 13:05:39 +0800

    Add procedures

--- FibonacciSumOfEvenNumLessFourMillion/Program.cs
+++ FibonacciSumOfEvenNumLessFourMillion/Program.cs
@@ -10,6 +10,25 @@ namespace FibonacciSumOfEvenNumLessFourMillion
     {
         static void Main(string[] args)
         {
+
+            int intTotal = 0;
+            int intSequence = 1;
+            int intAdder = 1;
+
+            while(intSequence < 4000000)
+            {
+                intSequence = intSequence + intAdder;
+                intAdder = intSequence - intAdder;
+
+                //check if number is even
+                if (intSequence % 2 == 0)
+                    intTotal = intTotal + intSequence;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("====>>>>>>>> Fibonacci Sum Of Even Numbers Less than Four Million: " + intTotal);
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
         }
     }
 }
